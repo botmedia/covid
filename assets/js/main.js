2ea7a2aacdbc2c79ce1005290269b60122d7ab81
botmedia <63251407+botmedia@users.noreply.github.com>
2020-04-09 09:37:32 +0200

    Create main.js

---
+++ assets/js/main.js
@@ -0,0 +1,32 @@
+function showPopup() {
+    window.bChat = {
+        "showChatNote": false,
+        'image': 'https://cdn.covid.chat/logo.png',
+        'text': 'Dôležité informácie o koronavíruse (COVID-19)',
+        "autoOpen": false,
+        "noAutoInit": false,
+        "noHistory": true,
+        "exitButton": true,
+        "skipWelcome": true,
+        "force": true,
+        "ref": "welcome"
+    };
+    (function(d, s, id) {
+        var js, bjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s);
+        js.async = true; js.id = id;
+        js.src = "https://cdn.covid.chat/chat.js";
+        bjs.parentNode.insertBefore(js, bjs);
+    }(document, 'script', 'bmedia'));
+}
+
+function showSample() {
+    document.getElementById("sample").innerHTML = "<iframe\n" +
+        "    style=\"border:0;width:100%;max-width:400px;height:430px;\"\n" +
+        "    src=\"https://covid.chat/chat/?history=0&ref=welcome\"></iframe>";
+}
+
+function showSampleSource() {
+    document.getElementById("sample-source").style.display = 'block';
+}
